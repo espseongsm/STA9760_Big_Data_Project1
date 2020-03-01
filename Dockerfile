fb09c1794b60c58a7296935a5634ea8d47f255fe
Soonmo Seong <59265733+espseongsm@users.noreply.github.com>
2020-03-01 03:10:38 -0500

    page_size argument update

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.7
 
 WORKDIR /app
 
+ENV APP_KEY = 123
+
 COPY requirements.txt /app
 
 RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -7,10 +7,22 @@ import os
 import pandas as pd
 import numpy as np
 
+domain = "data.cityofnewyork.us"
+app_token = 'RXeJhe7A7KO2CTm4SURGWOu5y'
+identifier = "nc67-uf89"
+
 if __name__ == "__main__":
-	client = Socrata("data.cityofnewyork.us", 'RXeJhe7A7KO2CTm4SURGWOu5y')
+
+	client = Socrata(domain, app_token)
 	page_size = sys.argv[1]
-	results = client.get("nc67-uf89", limit=page_size, offset=page_size)
+	num_rows = 49063985
+	num_pages = sys.argv[2]
+
+	if len(sys.argv[2]) > 0:
+		results = client.get("nc67-uf89", limit = int(num_pages)*int(page_size))
+	else: 
+		results = client.get("nc67-uf89", limit = num_rows)
+
 	df = pd.DataFrame.from_dict(results)
 	df.to_json ('results.json')
 	
