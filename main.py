b051c4d58c98c704757333b503f29c7265e8c9ef
Soonmo Seong <59265733+espseongsm@users.noreply.github.com>
2020-03-01 17:40:34 -0500

    add default values to argparse

--- main.py
+++ main.py
@@ -18,20 +18,19 @@ client = Socrata(domain, app_token)
 num_rows = 49063985
 
 #def main():
-parser = argparse.ArgumentParser(usage="main.py --page_size=page_size --num_pages=num_pages --output=output_filename", description="put the parameters")
+parser = argparse.ArgumentParser()#(usage="main.py --page_size=the_number_of_rows_in_a_page --num_pages=number_of_pages --output=filename", description="put the parameters")
 	
-parser.add_argument('--page_size', type=int, help="put the page size")
-parser.add_argument('--num_pages', type=int, help="put the number of pages")
-parser.add_argument('--output', type=str, help="put the name of output file")
+parser.add_argument('--page_size', type=int, default=1000, help="put the page size")
+parser.add_argument('--num_pages', type=int, default=4, help="put the number of pages")
+parser.add_argument('--output', type=str, default='results.json', help="put the name of output file")
 
 args = parser.parse_args()
 #print(args.page_size)
 #print(args.num_pages)
 #print(args.output)	
-if args.num_pages > 0:
-	results = client.get("nc67-uf89", limit = args.page_size*args.num_pages)
-else: 
-	results = client.get("nc67-uf89", limit = num_rows)
+
+#if args.num_pages > 0:
+results = client.get("nc67-uf89", limit = args.page_size*args.num_pages)
 
 df = pd.DataFrame.from_dict(results)
 df.to_json (args.output, orient='records', lines=True)
