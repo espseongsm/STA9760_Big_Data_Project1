12df809f2c8f46eb32b701c7fa90d80e8a8efe5e
Soonmo Seong <59265733+espseongsm@users.noreply.github.com>
2020-03-01 01:26:48 -0500

    create json file

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from sodapy import Socrata
 from requests import get
 import sys
 import json
+import requests
+import os
+import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
 	client = Socrata("data.cityofnewyork.us", 'RXeJhe7A7KO2CTm4SURGWOu5y')
 	page_size = sys.argv[1]
-	results = client.get("nc67-uf89", limit=page_size)
-	print(results)
+	results = client.get("nc67-uf89", limit=page_size, offset=page_size)
+	df = pd.DataFrame.from_dict(results)
+	df.to_json ('results.json')
+	
 
