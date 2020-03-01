6b0c76a7530cdedbfeaf257f9a5cc9fd82315d75
Soonmo Seong <59265733+espseongsm@users.noreply.github.com>
2020-03-01 00:49:41 -0500

    sys.argv update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from sodapy import Socrata
+from requests import get
+import sys
+import json
 
 if __name__ == "__main__":
 	client = Socrata("data.cityofnewyork.us", 'RXeJhe7A7KO2CTm4SURGWOu5y')
+	page_size = sys.argv[1]
+	results = client.get("nc67-uf89", limit=page_size)
+	print(results)
 
-	result = client.get("nc67-uf89", limit=page_size*num_pages)
-	print(result.json())
