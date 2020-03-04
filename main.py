859564de22cd271351d4bcf7ba1f103c1e1a3d13
Soonmo Seong <59265733+espseongsm@users.noreply.github.com>
2020-03-03 19:39:20 -0500

    handled the default value of num_pages, convert list to str to int

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ identifier = "nc67-uf89"
 #if __name__ == "__main__":
 
 client = Socrata(domain, app_token)
-row_limit_by_sodapy = 50000
+data_size = client.get(identifier, select='COUNT(*)')[0]
+num_rows = int(str(data_size)[11:19])
 
 #def main():
 parser = argparse.ArgumentParser()#(usage="main.py --page_size=the_number_of_rows_in_a_page --num_pages=number_of_pages --output=filename", description="put the parameters")
 	
 parser.add_argument('--page_size', type=int, default=1000, help="put the page size")
-parser.add_argument('--num_pages', type=int, default=row_limit_by_sodapy, help="put the number of pages")
+parser.add_argument('--num_pages', type=int, default=0, help="put the number of pages")
 parser.add_argument('--output', type=str, default='results.json', help="put the name of output file")
 
 args = parser.parse_args()
@@ -30,7 +31,7 @@ args = parser.parse_args()
 #print(args.output)	
 
 if args.num_pages == 0:
-	results = client.get("nc67-uf89", limit = row_limit_by_sodapy)
+	results = client.get("nc67-uf89", limit = num_rows)
 else:
 	results = client.get("nc67-uf89", limit = args.page_size*args.num_pages)
 
